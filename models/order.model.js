8ba755005faf9917fc43850b706b201f2802d5ec
Ankesh Chopde <acchopde84@gmail.com>
2023-05-06 15:42:46 +0530

    commited server

--- models/order.model.js
+++ models/order.model.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-    user : { type: ObjectId, ref: 'User' },
-	 books : [{ type: ObjectId, ref: 'Book' }],
+    user : String,
+	 books : [],
 	 totalAmount: Number
 },{
     versionKey:false
